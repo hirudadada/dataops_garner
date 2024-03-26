fbbbf6c8a8fcd3a8046ea95d7b9088f7c636166d
hirudadada <hirudadada@gmail.com>
2024-03-26 17:47:03 +0800

    change(ingestion): collector changed to pool

--- services/ingestion/actors/collector.rb
+++ services/ingestion/actors/collector.rb
@@ -2,11 +2,27 @@
 
 module Ingestion
   module Actors
-    class Collector < Ingestion::Actor
-      include Deps['actions.start']
-      include Deps['actions.handle_fetched_job_logs']
-      include Deps['actions.handle_submitted']
-      include Deps['actions.handle_updated_as_collected']
+    class Collector < Garner::ActorPool
+      class Worker < Ingestion::Actor
+        include Deps['actions.start']
+        include Deps['actions.handle_fetched_job_logs']
+        include Deps['actions.handle_submitted']
+        include Deps['actions.handle_updated_as_collected']
+      end
+
+      actor_class Worker
+      size { settings.collector_pool_size }
     end
   end
 end
+
+# module Ingestion
+#   module Actors
+#     class Collector < Ingestion::Actor
+#       include Deps['actions.start']
+#       include Deps['actions.handle_fetched_job_logs']
+#       include Deps['actions.handle_submitted']
+#       include Deps['actions.handle_updated_as_collected']
+#     end
+#   end
+# end
--- system/providers/settings.rb
+++ system/providers/settings.rb
@@ -18,6 +18,7 @@ module Garner
 
       # setting :garner_env, default: 'development', constructor: Types::String.constructor { |value|.downcase }.enum('development', 'production') # rubocop:disable Layout/LineLength
       setting :simulator_pool_size, default: 3, constructor: Types::Integer.constrained(filled: true)
+      setting :collector_pool_size, default: 2, constructor: Types::Integer.constrained(filled: true)
     end
   end
 end
