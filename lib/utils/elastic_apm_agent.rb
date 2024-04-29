1a72e4559f497c7bb785127e3c2aec747b337e71
hirudadada <hirudadada@gmail.com>
2024-04-29 10:57:46 +0800

    change: moved utils

---
+++ lib/utils/elastic_apm_agent.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+module Utils
+  module ElasticApmAgent
+    def check_connection(url)
+      uri = URI(url)
+      response = Net::HTTP.get_response(uri)
+      raise "Elastic APM server is not reachable. Please check the server URL: #{url}" unless response.code == '200'
+
+      p 'Elastic APM server is reachable.'
+      true
+    end
+  end
+end
