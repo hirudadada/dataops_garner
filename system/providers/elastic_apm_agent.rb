7e82360d8413c973f56e704e593a2ab92eeb7118
hirudadada <hirudadada@gmail.com>
2024-05-02 09:44:39 +0800

    feat: elastic and persistence password encryption

--- system/providers/elastic_apm_agent.rb
+++ system/providers/elastic_apm_agent.rb
@@ -2,12 +2,11 @@
 
 require 'elastic-apm'
 
-require_relative '../../lib/utils/elastic_apm_agent'
+require_relative '../../lib/utils/elastic/check_connection'
 require_relative '../../lib/utils/config'
 
 module Garner
   App.register_provider(:elastic_apm_agent) do
-    include Utils::ElasticApmAgent
     include Utils::Config
 
     prepare do
@@ -15,22 +14,19 @@ module Garner
     end
 
     start do
-      secret_token = nil
-      if target['settings'].apm_secret_token_encrypted.nil? || target['settings'].apm_secret_token_encrypted.empty?
-        secret_token = target['settings'].apm_secret_token
-      else
-        secret_token = Schematic::Cipher.new.decrypt(target['settings'].apm_secret_token_encrypted)
-      end
+      Utils::Elastic::CheckConnection.new.call(target['settings'].apm_server_url, target['settings'].apm_server_ca_cert_file)
 
+      server_url = target['settings'].apm_server_url
       config = {
         service_name: "#{format_for_path(target['settings'].db_host)}_#{format_for_path(target['settings'].db_name)}",
-        server_url: target['settings'].apm_server_url,
-        secret_token: ,
+        server_url:,
+        secret_token: target['settings'].apm_secret_token_encrypted.to_s.strip.empty? ? target['settings'].apm_secret_token : Schematic::Cipher.new.decrypt(target['settings'].apm_secret_token_encrypted),
         logger: target['logger']
       }
 
+      config[:server_ca_cert_file] = target['settings'].apm_server_ca_cert_file unless target['settings'].apm_server_ca_cert_file.nil?
+
       agent = ElasticAPM.start(config)
-      check_connection(target['settings'].apm_server_url) || check_connection(ENV['ELASTIC_APM_SERVER_URL']) # rubocop:disable Style/FetchEnvVar
       agent
     end
 
--- system/providers/settings.rb
+++ system/providers/settings.rb
@@ -3,54 +3,57 @@
 require 'dry-types'
 
 module Garner
-  Dry::Types.load_extensions(:maybe)
-
   App.register_provider(:settings, from: :dry_system) do # rubocop:disable Metrics/BlockLength
     settings do # rubocop:disable Metrics/BlockLength
+      # General Settings
       setting :app_name, default: Garnet.app.app_name, constructor: Types::String.constrained(filled: true)
       setting :app_env, default: :production, constructor: Types::Symbol
         .constructor { |value| value.to_s.downcase.to_sym }
         .enum(:development, :test, :production)
-
       setting :log_level, default: 'debug', constructor: Types::String.constrained(filled: true)
       setting :log_formatter, default: 'string', constructor: Types::String.constrained(filled: true)
 
+      # DB
       setting :db_host, constructor: Types::String.constrained(filled: true)
       setting :db_name, constructor: Types::String.constrained(filled: true)
       setting :db_user, constructor: Types::String.constrained(filled: true)
-      setting :db_password, constructor: Types::String.constrained(filled: true)
+      setting :db_password, constructor: Types::Optional::String
       setting :db_password_encrypted, constructor: Types::Optional::String
       setting :database_url, constructor: Types::String.constrained(filled: true)
       setting :enable_sql_log, default: false, constructor: Types::Params::Bool.constrained(filled: true)
       setting :use_named_schema, default: false, constructor: Types::Params::Bool.constrained(filled: true)
 
+      # Apm
       setting :apm_server_url, constructor: Types::String.constrained(filled: true)
-      setting :apm_secret_token, constructor: Types::String.constrained(filled: true)
+      setting :apm_secret_token, constructor: Types::Optional::String
       setting :apm_secret_token_encrypted, constructor: Types::Optional::String
+      setting :apm_server_ca_cert_file, constructor: Types::Optional::String
 
+      # Simulation Job
+      setting :simulator_pool_size, default: 3, constructor: Types::Coercible::Integer.constrained(gteq: 1)
       setting :simulation_job_name, default: 'simulation_job', constructor: Types::Optional::String
       setting :simulation_job_steps, default: 5, constructor: Types::Optional::Coercible::Integer.constrained(gteq: 1)
       setting :simulation_job_max_step_duration, default: 0.1, constructor: Types::Optional::Coercible::Float.constrained(gteq: 0.1)
       setting :simulation_job_error_rate, default: 0.01,
                                           constructor: Types::Optional::Coercible::Float.constrained(gteq: 0.0, lteq: 1.0)
-      # Opt this env out if you want the job to run endlessly.
-      setting :simulation_job_max_batches, default: Float::INFINITY,
-                                           constructor: Types::Optional::Coercible::Float.constrained(gteq: 1)
       setting :simulation_job_batch_size, default: 10, constructor: Types::Optional::Coercible::Integer.constrained(gteq: 1)
       setting :simulation_job_batch_wait, default: 20, constructor: Types::Optional::Coercible::Float.constrained(gteq: 0.1)
       setting :simulation_job_iterations, default: 3, constructor: Types::Optional::Coercible::Integer.constrained(gteq: 1)
-
-      setting :ingestion_job_name, default: 'ingestion_job', constructor: Types::Optional::String
       # Opt this env out if you want the job to run endlessly.
-      setting :ingestion_job_max_batches, default: Float::INFINITY,
-                                          constructor: Types::Optional::Coercible::Float.constrained(gteq: 1)
+      setting :simulation_job_max_batches, default: Float::INFINITY,
+                                           constructor: Types::Optional::Coercible::Float.constrained(gteq: 1)
+
+      # Ingestion Job
+      setting :collector_pool_size, default: 3, constructor: Types::Coercible::Integer.constrained(gteq: 1)
+      setting :ingestion_job_name, default: 'ingestion_job', constructor: Types::Optional::String
       setting :ingestion_job_batch_size, default: 20, constructor: Types::Coercible::Integer.constrained(gteq: 1)
       setting :ingestion_job_slice_size, default: 5, constructor: Types::Coercible::Integer.constrained(gteq: 1)
       setting :ingestion_job_batch_wait, default: 20, constructor: Types::Coercible::Float.constrained(gteq: 0.1)
+      # Opt this env out if you want the job to run endlessly.
+      setting :ingestion_job_max_batches, default: Float::INFINITY,
+                                          constructor: Types::Optional::Coercible::Float.constrained(gteq: 1)
 
-      setting :simulator_pool_size, default: 3, constructor: Types::Coercible::Integer.constrained(gteq: 1)
-      setting :collector_pool_size, default: 3, constructor: Types::Coercible::Integer.constrained(gteq: 1)
-
+      # Generic Job Settings
       setting :sleep_interval, default: 10, constructor: Types::Coercible::Integer.constrained(gteq: 1)
     end
   end
