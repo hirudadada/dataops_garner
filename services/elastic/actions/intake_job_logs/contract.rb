c53ce02f75897bd54cd3dd6c4fc44e5300bd0b33
hirudadada <hirudadada@gmail.com>
2024-05-07 00:54:36 +0800

    update intake contract

--- services/elastic/actions/intake_job_logs/contract.rb
+++ services/elastic/actions/intake_job_logs/contract.rb
@@ -6,7 +6,19 @@ module Elastic
       class Contract < Garnet::Contract
         schema do
           required(:slice).filled(:string)
-          required(:job_logs).filled(:array)
+          required(:job_logs).array(:hash) do
+            required(:id).filled(:integer)
+            required(:name).filled(:string)
+            required(:started_at).filled(:time)
+            required(:ended_at).filled(:time)
+            required(:job_step_logs).array(:hash) do
+              required(:name).filled(:string)
+              required(:started_at).filled(:time)
+              required(:ended_at).filled(:time)
+              # This could be error from the db layer
+              optional(:error).maybe(:string)
+            end
+          end
         end
       end
     end
