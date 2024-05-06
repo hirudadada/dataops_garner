e778efde01de09faa9eea6f91497c500e73524bf
hirudadada <hirudadada@gmail.com>
2024-05-07 00:58:45 +0800

    update persistence

--- lib/app/persistence/relations/job_logs.rb
+++ lib/app/persistence/relations/job_logs.rb
@@ -4,26 +4,40 @@ module Garner
   module Persistence
     module Relations
       class JobLogs < ROM::Relation[:sql]
-        schema(:job_logs, infer: false) do
-          attribute :joblogid, Types::Integer.meta(primary_key: true), as: :id
-          attribute :etl_procedure, Types::String, as: :name
-          attribute :etl_parameter, Types::String, as: :parameter
-          attribute :etl_starttime, Types::Time, as: :started_at
-          attribute :etl_completetime, Types::Time, as: :ended_at
-          attribute :etl_status, Types::Integer, as: :status
-          attribute :etl_status_description, Types::String, as: :status_description
-          attribute :etl_execute_by, Types::String, as: :executed_by
-          attribute :etl_batch_id, Types::String, as: :batch_id
-          attribute :records_insert_datetime, Types::Time, as: :created_at
-          attribute :logs_collected, Types::Bool, as: :collected
+        # TODO: still have to figure out usage of alias: value
+        schema(:job_status_log, infer: true,as: :job_logs) do
+          attribute :joblogid, Types::Integer.meta(primary_key: true, alias: :id)
+          attribute :etl_procedure, Types::String.meta(alias: :name)
+          # attribute :etl_parameter, Types::String.meta(alias: :parameter)
+          attribute :etl_starttime, Types::Time.meta(alias: :started_at)
+          attribute :etl_completetime, Types::Time.meta(alias: :ended_at)
+          # attribute :etl_status, Types::Integer.meta(alias: :status)
+          # attribute :etl_status_description, Types::String.meta(alias: :status_description)
+          # attribute :etl_execute_by, Types::String.meta(alias: :executed_by)
+          # attribute :etl_batch_id, Types::String.meta(alias: :batch_id)
+          attribute :records_insert_datetime, Types::Time.meta(alias: :created_at)
+          attribute :logs_collected, Types::Bool.meta(alias: :collected)
 
           associations do
-            has_many :job_step_logs, foreign_key: :joblogid
+            has_many :job_step_logs, foreign_key: :joblogid, combine_key: :joblogid, view: :for_job_status_log, override: true
           end
         end
 
+        def collectable
+          where(logs_collected: false).exclude(etl_completetime: nil)
+        end
+
+        def by_started_at
+          order(:etl_starttime)
+        end
+
+        # TODO: Need to find a way to combine the data from :job_details_logs
+        def with_details
+          relation(dataset: dataset.join(:DW_ETL_LOG__JOB_DETAILS_LOG, joblogid: :joblogid))
+        end
+
         dataset do
-          from(Sequel[:DW_ETL_LOG][:Job_Status_Log])
+          from(Sequel.qualify(:DW_ETL_LOG, :JOB_STATUS_LOG))
         end
       end
     end
--- lib/app/persistence/relations/job_step_logs.rb
+++ lib/app/persistence/relations/job_step_logs.rb
@@ -4,23 +4,28 @@ module Garner
   module Persistence
     module Relations
       class JobStepLogs < ROM::Relation[:sql]
-        schema(:job_step_logs, infer: false) do
-          attribute :stepid, Types::Integer.meta(primary_key: true), as: :id
-          attribute :joblogid, Types::Integer, as: :job_log_id
-          attribute :step, Types::String, as: :name
-          attribute :step_status, Types::String, as: :status
-          attribute :endtime, Types::Time, as: :ended_at
-          attribute :updatedrecordcount, Types::Integer, as: :updated_record_count
-          attribute :errormsg, Types::String, as: :error
-          attribute :remark, Types::String.optional, as: :remark
+        schema(:job_details_log, infer: true, as: :job_step_logs) do
+          attribute :stepid, Types::Integer.meta(primary_key: true, alas: :id)
+          attribute :joblogid, Types::Integer.meta(alias: :job_log_id)
+          attribute :step, Types::String.meta(alias: :name)
+          attribute :step_status, Types::String.meta(alias: :status)
+          attribute :endtime, Types::Time.meta(alias: :ended_at)
+          # attribute :updatedrecordcount, Types::Integer.meta(alias: :updated_record_count)
+          attribute :errormsg, Types::String.meta(alias: :error)
+          attribute :remark, Types::String.optional
 
           associations do
-            belongs_to :job_logs, foreign_key: :joblogid, view: :job_logs
+            belongs_to :job_logs, foreign_key: :joblogid, view: :with_details, override: true, combine_key: :joblogid
           end
         end
 
+        def for_job_status_log(_assoc, job_status_logs)
+          # TODO: This combine through ram, still have to figure out how to join by qualified name
+          where(joblogid: job_status_logs.map { |log| log[:joblogid] })
+        end
+
         dataset do
-          from(Sequel[:DW_ETL_LOG][:Job_Details_Log])
+          from(Sequel.qualify(:dw_etl_log, :job_details_log))
         end
       end
     end
