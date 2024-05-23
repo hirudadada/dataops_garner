06facc7f4bc724f21594c5feb54131dd29c890ae
hirudadada <hirudadada@gmail.com>
2024-05-23 18:24:45 +0800

    fix: lint errors

--- lib/utils/persistence/check_connection.rb
+++ lib/utils/persistence/check_connection.rb
@@ -4,9 +4,9 @@ module Utils
   module Persistence
     def self.check_connection(rom)
       rom.relations[:job_logs]
-        .limit(1)
-        .combine(:job_step_logs)
-        .to_a
+         .limit(1)
+         .combine(:job_step_logs)
+         .to_a
     end
   end
 end
